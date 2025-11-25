f0dd91a79f3fb857d2804adb7a77c5f408cf45a8
Mitch Marino <mitchellmarino-@Mitchells-MacBook-Pro-2.local>
2025-11-24 22:18:49 -0800

    Build development environment by configuring tsconfig.json and installing cors, nodemon, and ts-node

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express'; 
+import cors from 'cors'; 
 import type { Request, Response } from 'express'; 
 
 const app = express(); 
 const port = 8000; 
+
+// enable cors for cross origin resource sharing
+app.use(cors()); 
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello from Express with TypeScript!'); 
