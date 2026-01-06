cb99fe943ebbd339b57222629daa9079d76b1fa1
Mitch Marino <mitchellmarino-@Mitchells-MacBook-Pro-2.local>
2026-01-06 12:06:52 -0800

    installed dotenv, set up database connection with postgreSQL

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express'; 
 import cors from 'cors'; 
 import type { Request, Response } from 'express'; 
+import { pool } from "./db/pool.js";
 
 const app = express(); 
-const port = 8000; 
+const port = process.env.PORT || 8000; 
 
 // enable cors for cross origin resource sharing
 app.use(cors()); 
@@ -11,6 +12,11 @@ app.use(cors());
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello from Express with TypeScript!'); 
 }); 
+
+app.get("/health", async (_req, res) => {
+  const result = await pool.query("SELECT 1");
+  res.json({ ok: true, db: result.rowCount === 1 });
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`); 
